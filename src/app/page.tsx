b98ddd4600da8640f7b50f2a09f27ed60160a9cd
RashingPro <rashingpro@yandex.ru>
2025-04-19 13:33:46 +0300

    checkpoint commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,18 @@ export default async function Page() {
     const date = new Date();
     const dateBirth = new Date(2011, 8, 13)
     const age = new Date(date.getTime() - dateBirth.getTime()).getFullYear() - 1970
+
+    let wakatime;
+    const wakatimeRes = await fetch("https://api.wakatime.com/api/v1/users/b1c25f74-0bce-4dc9-bd10-50b9bb1f9d05/all_time_since_today")
+    console.log(wakatimeRes.status)
+    if (wakatimeRes.ok) {
+        wakatime = (await wakatimeRes.json())["total_seconds"]
+    }
+    console.log(wakatime)
     return <AppLayout>
         <div className={"main-info-container"}>
             <ProfileCard />
-            <ProfileDescription age={age}/>
+            <ProfileDescription age={age} wakatime={wakatime}/>
         </div>
 
     </AppLayout>
--- src/modules/ProfileDescription/module.tsx
+++ src/modules/ProfileDescription/module.tsx
@@ -3,12 +3,14 @@
 import "./module.css";
 
 interface ModuleProps {
-    age?: number
+    age?: number,
+    wakatime?: number
 }
 
-export default async function Module({age = 13}: ModuleProps) {
+export default async function Module({age = 13, wakatime}: ModuleProps) {
     return <div className={"profile-description-container"}>
         <h1>Привет!</h1>
         Я Кирилл, мне {age} лет, я программист из Санкт-Петербурга. Разрабатываю сайты, приложения, ботов и др. Полный список навыков ниже.
+        {wakatime != undefined && <>{Math.floor(wakatime/(60*60))}</>}
     </div>
 }

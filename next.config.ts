dcd69bb696467ffa7d7ba6bb2507edd47196d351
RashingPro <rashingpro@yandex.ru>
2025-04-19 13:01:59 +0300

    Add OWM to next.config to fetch weather icon

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    reactStrictMode: false
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "openweathermap.org",
+                pathname: "/img/wn/**"
+            }
+        ]
+    }
 };
 
 export default nextConfig;

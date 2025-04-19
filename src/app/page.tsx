c653d8b94c45cca186ffc154183f95dfc215c4af
RashingPro <rashingpro@yandex.ru>
2025-04-19 13:24:59 +0300

    Some dev progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@
 import ProfileCard from "@/modules/ProfileCard/module"
 import ProfileDescription from "@/modules/ProfileDescription/module"
 import AppLayout from "@/modules/AppLayout/module"
+import "./style.css"
 
 
 export default async function Page() {
+    const date = new Date();
+    const dateBirth = new Date(2011, 8, 13)
+    const age = new Date(date.getTime() - dateBirth.getTime()).getFullYear() - 1970
     return <AppLayout>
         <div className={"main-info-container"}>
             <ProfileCard />
-            <ProfileDescription />
+            <ProfileDescription age={age}/>
         </div>
 
     </AppLayout>
--- src/modules/ProfileDescription/module.tsx
+++ src/modules/ProfileDescription/module.tsx
@@ -1,7 +1,14 @@
 "use server"
 
-export default async function Module() {
-    return <div className={"profile-description-container"}>
+import "./module.css";
 
+interface ModuleProps {
+    age?: number
+}
+
+export default async function Module({age = 13}: ModuleProps) {
+    return <div className={"profile-description-container"}>
+        <h1>Привет!</h1>
+        Я Кирилл, мне {age} лет, я программист из Санкт-Петербурга. Разрабатываю сайты, приложения, ботов и др. Полный список навыков ниже.
     </div>
 }
